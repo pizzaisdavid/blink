60139f6583ed3f1791752d4ea4f9bf7d14c5f339
David Germain <davidispizza@gmail.com>
2018-08-23 21:30:25 -0400

    Add example

--- examples/switch.ts
+++ examples/switch.ts
@@ -4,11 +4,12 @@ import { Button, Light } from '../src'
 Promise.all([
   Button.make(2),
   Light.make(3),
-]).then(([button, led]: [Button, Light]) => {
-  button.pressed(() => {
-    led.flip()
-      .then(() => console.log(`the led state is ${led.state}`))
-      .catch((error) => console.error(error));
-  });
-})
-.catch((error) => console.error(error));
+])
+  .then(([button, led]: [Button, Light]) => {
+    button.pressed(() => {
+      led.flip()
+        .then(() => console.log(`the led state is ${led.state}`))
+        .catch((error) => console.error(error));
+    });
+  })
+  .catch((error) => console.error(error));
